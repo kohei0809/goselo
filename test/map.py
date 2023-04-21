b33f46596e26dd47686103abc675f1238ac961ab
kohei <matsumoto.k.bs@m.titech.ac.jp>
2023-04-21 15:49:38 +0900

    マップの回転・リサイズまで

--- test/map.py
+++ test/map.py
@@ -1,14 +1,20 @@
 import cv2
 import random
+import numpy as np
+import math
+import scipy
 
 #GOSELOマップの変換テスト
 env = 'default'
 map_path = '../map_images/' + env + '.png'
+change_color = True
+
+target_imsize = (224, 224)
 
 ########################
 # load map imformation #
 ########################
-im_map = cv2.imread(map_path) #[縦][横][RGB]
+im_map = cv2.imread(map_path) #[縦][横][BGR]
 #print(im_map)
 #マップを表示
 #cv2.imshow('map',im_map)
@@ -42,26 +48,153 @@ while True:
     if (im_map[yA][xA][0] > 100) and (im_map[yB][yB][0] > 100):
         break
     
+ 
+xA=100; yA=70; xB=100; yB=100;    
 start = (xA, yA)
 goal = (xB, yB)
 print("start:" + str(start))
 print("goal:" + str(goal))
 
-#startを赤、goalを青
-for i in range(-2, 2):
-    for j in range(-2, 2):
-        #print("i=" + str(i) + ", j=" + str(j))
-        if(yA+i >= 0) and (xA+j >= 0) and (yA+i < m) and (xA+j < n):
-            if im_map[yA+i][xA+j][2] == 255:
-                im_map[yA+i][xA+j][1] = 0
-                im_map[yA+i][xA+j][2] = 0
-        if(yB+i >= 0) and (xB+j >= 0) and (yB+i < m) and (xB+j < n):
-            if im_map[yB+i][xB+j][1] == 255:
-                im_map[yB+i][xB+j][0] = 0
-                im_map[yB+i][xB+j][1] = 0
-
+if change_color:
+    #startを赤、goalを青
+    im_map_ = im_map
+    for i in range(-2, 2):
+        for j in range(-2, 2):
+            #print("i=" + str(i) + ", j=" + str(j))
+            if(yA+i >= 0) and (xA+j >= 0) and (yA+i < m) and (xA+j < n):
+                if im_map_[yA+i][xA+j][0] == 255:
+                    im_map_[yA+i][xA+j][0] = 0
+                    im_map_[yA+i][xA+j][1] = 0
+            if(yB+i >= 0) and (xB+j >= 0) and (yB+i < m) and (xB+j < n):
+                if im_map_[yB+i][xB+j][0] == 255:
+                    im_map_[yB+i][xB+j][1] = 0
+                    im_map_[yB+i][xB+j][2] = 0
+                    
+                
+            
+                    
+    cv2.imshow('map',im_map_)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
     
+if change_color:
+    #startを赤、goalを青
+    im_map_ = im_map
+    for i in range(-2, 2):
+        for j in range(-2, 2):
+            #print("i=" + str(i) + ", j=" + str(j))
+            if(yA+i >= 0) and (xA+j >= 0) and (yA+i < m) and (xA+j < n):
+                im_map_[yA+i][xA+j][0] = 255
+                im_map_[yA+i][xA+j][1] = 255
+            if(yB+i >= 0) and (xB+j >= 0) and (yB+i < m) and (xB+j < n):
+                im_map_[yB+i][xB+j][1] = 255
+                im_map_[yB+i][xB+j][2] = 255
+##################
+# expand the map #
+##################
+#白色の領域を収縮
+im_map_expand = cv2.erode(im_map, np.ones((2, 2), np.uint8), 1)
+#黒を1, 白を0にする
+ret, the_map = cv2.threshold(im_map, 100, 1, cv2.THRESH_BINARY_INV)
+#3次元目がRGB表記だったが、白or黒なので、1つだけにする
+the_map = the_map[:, :, 0]
 
-cv2.imshow('map',im_map)
-cv2.waitKey(0)
+###############
+# convert img #
+###############
+#the_map, start, goal, pathlogをまとめ、一気に回転、拡大をする
+orig_map = np.zeros((4, m, n), dtype=np.float32)
+orig_map[0] = np.array(the_map)
+orig_map[1][yA][xA] = 1
+orig_map[2][yB][xB] = 1
+orig_map[3] = np.array(the_map_pathlog)
+#(m, n, map)
+orig_map = orig_map.transpose((1, 2, 0)) 
+
+#xA, yAは毎回更新され現在地を示す
+sx = xA; sy = yA; gx = xB; gy = yB
+#現在地とゴールの中点
+mx = (sx + gx) / 2.
+my = (sy + gy) / 2.
+#画面の端からの最大の長さ
+dx_ = max(mx, orig_map.shape[1] - mx)
+dy_ = max(my, orig_map.shape[0] - my)
+im2 = np.zeros((int(dy_ * 2), int(dx_ * 2), orig_map.shape[2]), dtype=np.float32)
+im2[int(dy_-my):int(dy_-my)+orig_map.shape[0], int(dx_-mx):int(dx_-mx)+orig_map.shape[1]] = orig_map
+#壁を2にする(?)
+im2[im2 == 1] = 2
+if gx == sx:
+    if gy > sy:
+        theta = 90
+    else:
+        theta = 270
+else:
+    theta = math.atan(float(gy-sy) / float(gx-sx)) * 180 / math.pi
+    if gx-sx < 0:
+        theta = theta + 180
+        
+print("theta=" + str(theta))
+im2 = scipy.ndimage.interpolation.rotate(im2, theta+90)
+#(map, m, n)
+im2 = im2.transpose((2, 0, 1))
+#print(im2.shape)
+
+cv2.imshow('map',im2[0])
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#現在地からゴールまでの距離
+L = int(np.sqrt((gx-sx)*(gx-sx) + (gy-sy)*(gy-sy)))
+#[the_map, the_map_path_log]
+im2 = [im2[0], im2[3]]
+#(224, 224, 6)
+im3 = np.zeros((target_imsize[0], target_imsize[1], 6), dtype=np.uint8)
+#(6, 224, 224)
+im3 = im3.transpose((2, 0, 1))
+l = (L+4, 4*L, 8*L)
+for n_ in range(2):
+    for i in range(3):
+        im2_ = np.zeros((l[i], l[i]), dtype=np.uint8)
+        #int()をつけていいか微妙
+        y1 = int(max(0,  (im2[n_].shape[0]-l[i])/2))
+        y2 = int(max(0, -(im2[n_].shape[0]-l[i])/2))
+        x1 = int(max(0,  (im2[n_].shape[1]-l[i])/2))
+        x2 = int(max(0, -(im2[n_].shape[1]-l[i])/2))
+        #print("[0], [1], l[i] = " + str(im2[n_].shape[0]) + ", " + str(im2[n_].shape[1]) + ", " + str(l[i]))
+        
+        #8Lなどがマップからはみ出さないようにする
+        dy_ = min(l[i], im2[n_].shape[0])
+        dx_ = min(l[i], im2[n_].shape[1])
+        
+        #print("x1, dx_, y1, dy_ = " + str(x1) + ", " + str(dx_) + ", " + str(y1) + ", " + str(dy_))
+        #print("x2, dx_, y2, dy_ = " + str(x2) + ", " + str(dx_) + ", " + str(y2) + ", " + str(dy_))
+        
+        #該当する箇所だけコピー
+        im2_[y2:y2+dy_, x2:x2+dx_] = im2[n_][y1:y1+dy_, x1:x1+dx_]
+        #print(im3[i + n_*3].shape)
+        im3[i + n_*3] = cv2.resize(im2_, im3[i + n_*3].shape, interpolation=cv2.INTER_AREA)
+
+im3 = im3 * 0.5
+im3[(im3 > 0)*(im3 <= 1)] = 1
+
+#(224, 224, 6)
+input_map = im3.transpose((1, 2, 0))
+input_map = input_map / 255.
+
+#for i in range(20):
+    #print(im3[i].shape)
+    #print(im3[i])
+print(im3[0])
+cv2.imshow('map',im3[0])
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+print(im3[1].shape)
+cv2.imshow('map',im3[1])
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+print(im3[2].shape)
+cv2.imshow('map',im3[2])
+cv2.waitKey()
 cv2.destroyAllWindows()
